d29462800225c7d66e2a1b42a00d7a6bc51c0828
willuwontu <willuwontu3@gmail.com>
2021-10-15 13:56:55 -0700

    v 1.2.8 cont

---
+++ Wills Wacky Cards/Cards/Curses/Template.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using WillsWackyCards.Extensions;
+using WillsWackyCards.MonoBehaviours;
+using WillsWackyManagers.Utils;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using UnityEngine;
+
+namespace WillsWackyCards.Cards.Curses
+{
+    class Template : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
+        {
+            UnityEngine.Debug.Log($"[WWC][Curse] {GetTitle()} Built");
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            UnityEngine.Debug.Log($"[WWC][Curse] {GetTitle()} added to Player {player.playerID}");
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            UnityEngine.Debug.Log($"[WWC][Curse] {GetTitle()} removed from Player {player.playerID}");
+        }
+
+        protected override string GetTitle()
+        {
+            return "Misfire";
+        }
+        protected override string GetDescription()
+        {
+            return "Bippity boop, your gun can no longer shoot.";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Effect",
+                    amount = "No",
+                    simepleAmount = CardInfoStat.SimpleAmount.aLotOf
+                }
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.DestructiveRed;
+        }
+        public override string GetModName()
+        {
+            return "Curse";
+        }
+        public override bool GetEnabled()
+        {
+            return true;
+        }
+    }
+}
