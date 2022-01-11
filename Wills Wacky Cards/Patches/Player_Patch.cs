1c49313fae30f4784781f45af4723bb4c3e2b200
willuwontu <willuwontu3@gmail.com>
2022-01-10 18:27:28 -0800

    V 1.6.8

---
+++ Wills Wacky Cards/Patches/Player_Patch.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using WWC.Extensions;
+using WWC.MonoBehaviours;
+using Sonigon;
+using UnboundLib;
+using WWC.UI;
+
+namespace WWC.Patches
+{
+    [HarmonyPatch(typeof(Player))] 
+    class Player_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("Start")]
+        static void Spotlight(Player __instance)
+        {
+            if (__instance.data.view.IsMine)
+            {
+                PlayerSpotlight.AddSpotToPlayer(__instance);
+            }
+        }
+
+        //[HarmonyPrefix]
+        //[HarmonyPatch("SomeMethod")]
+        //static void MyMethodName()
+        //{
+
+        //}
+
+        //[HarmonyPostfix]
+        //[HarmonyPatch("SomeMethod")]
+        //static void MyMethodName()
+        //{
+
+        //}
+    }
+}
