b779e0b8636d018e16807726aafe9342424b7068
willuwontu <willuwontu3@gmail.com>
2022-01-21 09:08:13 -0800

    V 1.6.13

--- Wills Wacky Cards/WillsWackyCards.cs
+++ Wills Wacky Cards/WillsWackyCards.cs
@@ -40,7 +40,7 @@ namespace WWC
     {
         private const string ModId = "com.willuwontu.rounds.cards";
         private const string ModName = "Will's Wacky Cards";
-        public const string Version = "1.6.12"; // What version are we on (major.minor.patch)?
+        public const string Version = "1.6.13"; // What version are we on (major.minor.patch)?
 
         public const string ModInitials = "WWC";
         public const string CurseInitials = "Curse";
