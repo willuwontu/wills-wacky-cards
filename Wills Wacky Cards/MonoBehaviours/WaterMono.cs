f9b9afc2b51106224abe6d6c86bd909c2d56310a
willuwontu <willuwontu3@gmail.com>
2022-01-24 08:28:03 -0800

    Update WaterMono.cs

--- Wills Wacky Cards/MonoBehaviours/WaterMono.cs
+++ Wills Wacky Cards/MonoBehaviours/WaterMono.cs
@@ -112,11 +112,14 @@ namespace WaterMapObjects.MonoBehaviours
     public class WaterMono : MonoBehaviour
     {
         TrailRenderer trail;
+
+        public RaycastHit2D[] hits;
+
         public virtual float forceMult 
         {
             get
             {
-                return 0.75f;
+                return 0.74f;
             }
         }
         public virtual Color waterColor 
@@ -161,31 +164,7 @@ namespace WaterMapObjects.MonoBehaviours
         //    //    return;
         //    //}
 
-        //    trail.AddPositions(new Vector3[] { this.transform.position, collider.transform.position});
-
-        //    // If Player
-        //    if (collider.GetComponent<Player>())
-        //    {
-        //        HandlePlayer(collider.GetComponent<Player>());
-
-        //        return;
-        //    }
-
-        //    // If Bullet
-        //    if (collider.GetComponentInParent<ProjectileHit>())
-        //    {
-        //        HandleBullet(collider.GetComponentInParent<ProjectileHit>());
-
-        //        return;
-        //    }
-
-        //    // If Box
-        //    if (collider.GetComponent<Rigidbody2D>())
-        //    {
-        //        HandleBox(collider.GetComponent<Rigidbody2D>());
-
-        //        return;
-        //    }
+        //    HandleCollision(collider);
         //}
 
         private void FixedUpdate()
@@ -195,85 +174,42 @@ namespace WaterMapObjects.MonoBehaviours
                 return;
             }
 
-            // Stupid raycast method to do a terrible job of brute forcing the intersecting area.
-            if (true)
+            var tempHits = new List<RaycastHit2D>();
+
+            var tempCorners = new Vector3[4];
+            this.gameObject.GetOrAddComponent<RectTransform>().GetWorldCorners(tempCorners);
+            Vector2[] waterCorners = tempCorners.Select(corner => (Vector2)corner).ToArray();
+
+            trail.SetPositions(tempCorners);
+            trail.AddPosition(tempCorners[0]);
+
+            // Get all collisions going clockwise
+            for (int i = 0; i < waterCorners.Length; i++)
             {
-                List<RaycastHit2D> hits = new List<RaycastHit2D>();
-                var tempCorners = new Vector3[4];
-                this.gameObject.GetOrAddComponent<RectTransform>().GetWorldCorners(tempCorners);
-                Vector2[] waterCorners = tempCorners.Select(corner => (Vector2)corner).ToArray().OrderBy(corner => corner.x).ToArray();
-
-                // the first corner will always be the leftmost
-                var raySpacing = 0.05f < (waterCorners[3].x - waterCorners[0].x) / 100f ? 0.05f : (waterCorners[3].x - waterCorners[0].x) / 100f;
-                for (float x = (waterCorners[0].x + raySpacing); x < waterCorners[3].x; x += raySpacing)
+                if (i == 0)
                 {
-                    Vector2[] line1 = new Vector2[2];
-                    // line 1
-                    if (x < waterCorners[1].x)
-                    {
-                        line1[0] = waterCorners[0];
-                        line1[1] = waterCorners[1];
-                    }
-                    else
-                    {
-                        line1[0] = waterCorners[1];
-                        line1[1] = waterCorners[3];
-                    }
-
-                    Vector2[] line2 = new Vector2[2];
-                    // line 2
-                    if (x < waterCorners[2].x)
-                    {
-                        line1[0] = waterCorners[0];
-                        line1[1] = waterCorners[2];
-                    }
-                    else
-                    {
-                        line1[0] = waterCorners[2];
-                        line1[1] = waterCorners[3];
-                    }
-
-                    var dir1 = (line1[1] - line1[0]).normalized;
-                    var dir2 = (line2[1] - line2[0]).normalized;
-
-                    var m1 = dir1.y / dir1.x;
-                    var m2 = dir2.y / dir2.x;
-
-                    var b1 = line1[0].y - m1 * line1[0].x;
-                    var b2 = line2[0].y - m2 * line2[0].x;
-
-                    var p1 = m1 * x + b1;
-                    var p2 = m2 * x + b2;
-
-                    hits.AddRange(Physics2D.RaycastAll((p1.y < p2.y ? p1 : p2), Vector2.up, Mathf.Abs(p1.y - p2.y)).ToList());
-                    hits.AddRange(Physics2D.RaycastAll((p1.y > p2.y ? p1 : p2), Vector2.down, Mathf.Abs(p1.y - p2.y)).ToList());
-
-                    trail.AddPositions(new Vector3[] { p1, p2 });
+                    tempHits.AddRange(Physics2D.LinecastAll(waterCorners[waterCorners.Length-1], waterCorners[0]));
                 }
-
-                UnityEngine.Debug.Log($"{hits.Count()} hits found.");
-
-                Dictionary<Collider2D, List<RaycastHit2D>> colliderHits = new Dictionary<Collider2D, List<RaycastHit2D>>();
-
-                foreach (var hit in hits)
+                else
                 {
-                    if (colliderHits.ContainsKey(hit.collider))
-                    {
-                        colliderHits[hit.collider].Add(hit);
-                    }
-                    else
-                    {
-                        colliderHits.Add(hit.collider, new List<RaycastHit2D> { hit });
-                    }
-                }
-
-                //colliderHits = hits.Select(hit => hit.collider).Distinct().ToDictionary(collider => collider, collider => hits.Where(hit => hit.collider == collider).ToList());
-
-                foreach (var kvp in colliderHits)
-                {
-                    HandleRaycastCollision(kvp.Key, kvp.Value.ToArray());
+                    tempHits.AddRange(Physics2D.LinecastAll(waterCorners[i - 1], waterCorners[i]));
                 }
             }
+
+            // Get all collisions going counterclockwise
+            for (int i = 0; i < waterCorners.Length; i++)
+            {
+                if (i == 0)
+                {
+                    tempHits.AddRange(Physics2D.LinecastAll(waterCorners[0], waterCorners[waterCorners.Length-1]));
+                }
+                else
+                {
+                    tempHits.AddRange(Physics2D.LinecastAll(waterCorners[i], waterCorners[i-1]));
+                }
+            }
+
+            hits = tempHits.ToArray();
 
             var colliders = Physics2D.OverlapBoxAll(this.transform.position, this.gameObject.GetComponent<RectTransform>().localScale, Vector2.SignedAngle(Vector2.up, this.transform.up.normalized));
 
@@ -286,7 +222,13 @@ namespace WaterMapObjects.MonoBehaviours
         public virtual void HandleCollision(Collider2D collider)
         {
 
-            //trail.AddPositions(new Vector3[] { this.transform.position, collider.transform.position });
+            trail.AddPositions(new Vector3[] { this.transform.position, collider.transform.position });
+
+            // If our patch of water
+            if (collider.gameObject == this.gameObject)
+            {
+                return;
+            }
 
             // If Player
             if (collider.GetComponent<Player>())
@@ -308,36 +250,6 @@ namespace WaterMapObjects.MonoBehaviours
             if (collider.GetComponent<Rigidbody2D>())
             {
                 HandleBox(collider.GetComponent<Rigidbody2D>());
-
-                return;
-            }
-        }
-
-        public virtual void HandleRaycastCollision(Collider2D collider, RaycastHit2D[] hits)
-        {
-
-            trail.AddPositions(new Vector3[] { this.transform.position, collider.transform.position });
-
-            // If Player
-            if (collider.GetComponent<Player>())
-            {
-                HandlePlayer(collider.GetComponent<Player>());
-
-                return;
-            }
-
-            // If Bullet
-            if (collider.GetComponentInParent<ProjectileHit>())
-            {
-                HandleBullet(collider.GetComponentInParent<ProjectileHit>());
-
-                return;
-            }
-
-            // If Box
-            if (collider.GetComponent<Rigidbody2D>())
-            {
-                HandleBox(collider.GetComponent<Rigidbody2D>(), hits);
 
                 return;
             }
@@ -375,67 +287,50 @@ namespace WaterMapObjects.MonoBehaviours
 
         public virtual void HandleBox(Rigidbody2D rb)
         {
-            ///*
-            //1 - 2
-            //|   |
-            //0 - 3
-            //*/
+            /*
+            1 - 2
+            |   |
+            0 - 3
+            */
 
-            //var tempCorners = new Vector3[4];
-            //this.gameObject.GetOrAddComponent<RectTransform>().GetWorldCorners(tempCorners);
-            //Vector2[] waterCorners = tempCorners.Select(corner => (Vector2) corner).ToArray();
+            // get the boxes of each object
+            List<RaycastHit2D> hits = new List<RaycastHit2D>();
+            var tempCorners = new Vector3[4];
+            this.gameObject.GetOrAddComponent<RectTransform>().GetWorldCorners(tempCorners);
+            Vector2[] waterCorners = tempCorners.Select(corner => (Vector2)corner).ToArray();
 
-            //tempCorners = new Vector3[4];
-            //rb.gameObject.GetOrAddComponent<RectTransform>().GetWorldCorners(tempCorners);
-            //Vector2[] rigidCorners = tempCorners.Select(corner => (Vector2)corner).ToArray();
+            rb.gameObject.GetOrAddComponent<RectTransform>().GetWorldCorners(tempCorners);
+            Vector2[] rigidCorners = tempCorners.Select(corner => (Vector2)corner).ToArray();
 
-            //bool[] inWater = rigidCorners.Select(corner => PointIsInsideShape(waterCorners, corner)).ToArray();
+            // Find out if any corners are in the water
+            bool[] inWater = rigidCorners.Select(corner => PointIsInsideShape(waterCorners, corner)).ToArray();
 
-            //List<Vector2> inWaterShape = new List<Vector2>();
+            // If all of the corners are in the object, centroid is the centroid of the object.
+            if (!inWater.Contains(false))
+            {
+                rb.AddForce(Vector2.up * rb.mass * forceMult, ForceMode2D.Impulse);
+                return;
+            }
 
-            //// Go through each corner
-            //for (int i = 0; i < rigidCorners.Length; i++)
-            //{
-            //    // if the corner is in water
-            //    if (inWater[i])
-            //    {
-            //        // Add to list
-            //        inWaterShape.Add(rigidCorners[i]);
+            // Find out if any of the water's corners are in the object
+            bool[] inObject = waterCorners.Select(corner => PointIsInsideShape(rigidCorners, corner)).ToArray();
 
-            //        // If the previous corner wasn't in water, get the intersection
-            //        if ((i > 0 && !inWater[i - 0]) || (i == 0 && !inWater[inWater.Length]))
-            //        {
+            // If all the corners of the water are in the object, centroid is center of water region
+            if (!inObject.Contains(false))
+            {
+                rb.AddForceAtPosition(Vector2.up * rb.mass * forceMult, this.transform.position, ForceMode2D.Impulse);
+                return;
+            }
 
-            //        }
-            //    }
-            //    else if ((i > 0 && inWater[i - 1]) || (i == 0 && inWater[inWater.Length]))
-            //    {
-            //        var intersection = Vector2.zero;
+            // Get all contact points from our linecasts
+            var contactPoints = hits.Where(hit => hit.collider.gameObject == rb.gameObject).Select(hit => hit.point).ToList();
+            // Add any corners inside the water
+            contactPoints.AddRange(rigidCorners.Where(corner => PointIsInsideShape(waterCorners, corner)));
 
-            //        var x = 0;
-            //        while (!LineSegmentsIntersectionWithPrecisonControl())
-            //    }
-            //}
+            // Centroid is (sum x / count x, sum y / count y)
+            var centroid = new Vector2(contactPoints.Select(point => point.x).Sum() / contactPoints.Count(), contactPoints.Select(point => point.y).Sum() / contactPoints.Count());
 
-            //// Stupid raycast method to do a terrible job of brute forcing the intersecting area.
-            //{
-            //    List<RaycastHit2D> hits = new List<RaycastHit2D>();
-            //    var tempCorners = new Vector3[4];
-            //    this.gameObject.GetOrAddComponent<RectTransform>().GetWorldCorners(tempCorners);
-            //    Vector2[] waterCorners = tempCorners.Select(corner => (Vector2)corner).ToArray().OrderBy(corner => corner.x).ToArray();
-
-            //    tempCorners = new Vector3[4];
-            //    rb.gameObject.GetOrAddComponent<RectTransform>().GetWorldCorners(tempCorners);
-            //    Vector2[] rigidCorners = tempCorners.Select(corner => (Vector2)corner).ToArray().OrderBy(corner => corner.x).ToArray();
-            //    var rbMinX = rigidCorners.Select(corner => corner.x).Min();
-            //    var rbMaxX = rigidCorners.Select(corner => corner.x).Max();
-
-            //    // the first corner will always be the leftmost
-
-            //}
-
-            // Fallback method
-            rb.AddForce(Vector2.up * rb.mass * forceMult, ForceMode2D.Impulse);
+            rb.AddForceAtPosition(Vector2.up * rb.mass * forceMult, centroid, ForceMode2D.Impulse);
         }
 
         private bool PointIsInsideShape(Vector2[] outline, Vector2 point)
@@ -488,131 +383,6 @@ namespace WaterMapObjects.MonoBehaviours
 
                 return 1;
             }
-        }
-
-        /// <summary>
-        /// Calculate the intersection point of two line segments
-        /// </summary>
-        /// <param name="p1"></param>
-        /// <param name="p2"></param>
-        /// <param name="p3"></param>
-        /// <param name="p4"></param>
-        /// <param name="intersection"></param>
-        /// <param name="fSelfDefinedEpsilon">Epsilon in pixels</param>
-        /// <returns></returns>
-        public static bool LineSegmentsIntersectionWithPrecisonControl(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, ref Vector2 intersection, float fSelfDefinedEpsilon = 1.0f)
-        {
-            //Debug.Log (string.Format("LineSegmentsIntersection2 p1 {0} p2 {1} p3 {2} p4{3}", p1, p2, p3, p4)); // the float value precision in the log is just 0.0f
-            UnityEngine.Assertions.Assert.IsTrue(fSelfDefinedEpsilon > 0);
-
-            float Ax, Bx, Cx, Ay, By, Cy, d, e, f, num/*,offset*/;
-            float x1lo, x1hi, y1lo, y1hi;
-            Ax = p2.x - p1.x;
-            Bx = p3.x - p4.x;
-
-            // X bound box test/
-            if (Ax < 0)
-            {
-                x1lo = p2.x; x1hi = p1.x;
-            }
-            else
-            {
-                x1hi = p2.x; x1lo = p1.x;
-            }
-
-            if (Bx > 0)
-            {
-                if ((x1hi < p4.x && Mathf.Abs(x1hi - p4.x) > fSelfDefinedEpsilon)
-                    || (p3.x < x1lo && Mathf.Abs(p3.x - x1lo) > fSelfDefinedEpsilon))
-                    return false;
-            }
-            else
-            {
-                if ((x1hi < p3.x && Mathf.Abs(x1hi - p3.x) > fSelfDefinedEpsilon)
-                    || (p4.x < x1lo && Mathf.Abs(p4.x - x1lo) > fSelfDefinedEpsilon))
-                    return false;
-            }
-
-            Ay = p2.y - p1.y;
-            By = p3.y - p4.y;
-
-            // Y bound box test//
-            if (Ay < 0)
-            {
-                y1lo = p2.y; y1hi = p1.y;
-            }
-            else
-            {
-                y1hi = p2.y; y1lo = p1.y;
-            }
-
-            if (By > 0)
-            {
-                if ((y1hi < p4.y && Mathf.Abs(y1hi - p4.y) > fSelfDefinedEpsilon)
-                    || (p3.y < y1lo && Mathf.Abs(p3.y - y1lo) > fSelfDefinedEpsilon))
-                    return false;
-            }
-            else
-            {
-                if ((y1hi < p3.y && Mathf.Abs(y1hi - p3.y) > fSelfDefinedEpsilon)
-                    || (p4.y < y1lo && Mathf.Abs(p4.y - y1lo) > fSelfDefinedEpsilon))
-                    return false;
-            }
-
-            Cx = p1.x - p3.x;
-            Cy = p1.y - p3.y;
-            d = By * Cx - Bx * Cy;  // alpha numerator//
-            f = Ay * Bx - Ax * By;  // both denominator//
-
-            // alpha tests//
-
-            if (f > 0)
-            {
-                if ((d < 0 && Mathf.Abs(d) > fSelfDefinedEpsilon)
-                    || (d > f && Mathf.Abs(d - f) > fSelfDefinedEpsilon))
-                    return false;
-            }
-            else
-            {
-                if ((d > 0 && Mathf.Abs(d) > fSelfDefinedEpsilon)
-                    || (d < f && Mathf.Abs(d - f) > fSelfDefinedEpsilon))
-                    return false;
-            }
-            e = Ax * Cy - Ay * Cx;  // beta numerator//
-
-            // beta tests //
-
-            if (f > 0)
-            {
-                if ((e < 0 && Mathf.Abs(e) > fSelfDefinedEpsilon)
-                    || (e > f) && Mathf.Abs(e - f) > fSelfDefinedEpsilon)
-                    return false;
-            }
-            else
-            {
-                if ((e > 0 && Mathf.Abs(e) > fSelfDefinedEpsilon)
-                    || (e < f && Mathf.Abs(e - f) > fSelfDefinedEpsilon))
-                    return false;
-            }
-
-            // check if they are parallel
-            if (f == 0 && Mathf.Abs(f) > fSelfDefinedEpsilon)
-                return false;
-
-            // compute intersection coordinates //
-            num = d * Ax; // numerator //
-
-            //    offset = same_sign(num,f) ? f*0.5f : -f*0.5f;   // round direction //
-
-            //    intersection.x = p1.x + (num+offset) / f;
-            intersection.x = p1.x + num / f;
-            num = d * Ay;
-
-            //    offset = same_sign(num,f) ? f*0.5f : -f*0.5f;
-
-            //    intersection.y = p1.y + (num+offset) / f;
-            intersection.y = p1.y + num / f;
-            return true;
         }
     }
 
